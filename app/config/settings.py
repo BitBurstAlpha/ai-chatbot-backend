0ee31bbfdcfae46c25151d4aa313832f7f0d87af
BitBurstAlpha <dilshansamarakoon2001@gmail.com>
2025-04-10 02:15:39 +0530

    fix: ensure APP_NAME is loaded from environment variables in config

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,6 +7,7 @@ class Config:
     
     # Debug Mode
     DEBUG = os.getenv('FLASK_ENV', 'development') == 'development'
+    APP_NAME = os.getenv('APP_NAME')
 
 def get_config():
     return Config
